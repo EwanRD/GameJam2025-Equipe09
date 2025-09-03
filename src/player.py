43ac8a32692078a18ed2bc4a4c8977f935dc97da
micheleliot <eliotm38@gmail.com>
2025-09-03 17:46:17 +0200

    add assets

--- src/player.py
+++ src/player.py
@@ -9,14 +9,23 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]and not keys[pygame.K_DOWN] or keys[pygame.K_LEFT] and not keys[pygame.K_UP]:
+        if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT] and not keys[pygame.K_UP] and not keys[pygame.K_DOWN] :
             self.rect.x -= 5
-        if keys[pygame.K_RIGHT] and not keys[pygame.K_DOWN] or keys[pygame.K_RIGHT] and not keys[pygame.K_UP]:
+        if keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT] and not keys[pygame.K_UP] and not keys[pygame.K_DOWN] :
             self.rect.x += 5
-        if keys[pygame.K_UP] and not keys[pygame.K_LEFT] or keys[pygame.K_UP] and not keys[pygame.K_RIGHT]:
+        if keys[pygame.K_UP] and not keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT] and not keys[pygame.K_DOWN] :
             self.rect.y -= 5
-        if keys[pygame.K_DOWN] and not keys[pygame.K_LEFT] or keys[pygame.K_DOWN] and not keys[pygame.K_RIGHT]:
+        if keys[pygame.K_DOWN] and not keys[pygame.K_RIGHT] and not keys[pygame.K_UP] and not keys[pygame.K_LEFT] :
             self.rect.y += 5
-        if keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
-            self.rect.x -= 2
-            self.rect.y += 2
+        if keys[pygame.K_DOWN] and keys[pygame.K_LEFT]:
+            self.rect.x -= 4
+            self.rect.y += 4
+        if keys[pygame.K_DOWN] and keys[pygame.K_RIGHT]:
+            self.rect.x += 4
+            self.rect.y += 4
+        if keys[pygame.K_UP] and keys[pygame.K_RIGHT]:
+            self.rect.x += 4
+            self.rect.y -= 4
+        if keys[pygame.K_UP] and keys[pygame.K_LEFT]:
+            self.rect.x -= 4
+            self.rect.y -= 4
