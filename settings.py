6dc02437aa6a7ae55e910f1129ef4ca4e2678fa4
micheleliot <eliotm38@gmail.com>
2025-09-04 17:26:19 +0200

    feat: add shoot in all direction

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+from enum import Enum
+import math
+
 # Paramètres globaux
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 900
@@ -6,3 +9,36 @@ FPS = 60
 # Couleurs
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+# player attributes
+PLAYER_SPEED = 5
+PLAYER_HEALTH = 3
+PLAYER_DOMMAGE = 1
+PLAYER_COULDOWN = 0.5
+
+# Vitesse globale des projectiles
+PROJ_SPEED = 5
+PROJ_DIAG_SPEED = PROJ_SPEED / math.sqrt(2)
+
+class DIRECTION(Enum):
+    H  = (0, -PROJ_SPEED)        # Haut
+    B  = (0, PROJ_SPEED)         # Bas
+    G  = (-PROJ_SPEED, 0)        # Gauche
+    D  = (PROJ_SPEED, 0)         # Droite
+    HG = (-PROJ_DIAG_SPEED, -PROJ_DIAG_SPEED)  # Haut-Gauche
+    HD = (PROJ_DIAG_SPEED, -PROJ_DIAG_SPEED)   # Haut-Droite
+    BG = (-PROJ_DIAG_SPEED, PROJ_DIAG_SPEED)   # Bas-Gauche
+    BD = (PROJ_DIAG_SPEED, PROJ_DIAG_SPEED)    # Bas-Droite
+
+class ARROW_DIRECTION(Enum):
+    H  = "assets/sprites/Arrow/flechehaut.png"           # Haut
+    B  = "assets/sprites/Arrow/flechebas.png"           # Bas
+    G  = "assets/sprites/Arrow/flechegauche.png"        # Gauche
+    D  = "assets/sprites/Arrow/flechedroite.png"        # Droite
+    HG = "assets/sprites/Arrow/flechehautgauche.png"     # Haut-Gauche
+    HD = "assets/sprites/Arrow/flechehautdroite.png"     # Haut-Droite
+    BG =  "assets/sprites/Arrow/flechebasgauche.png"    # Bas-Gauche
+    BD = "assets/sprites/Arrow/flechebasdroite.png"     # Bas-Droite
+
+
+
--- src/player.py
+++ src/player.py
@@ -1,4 +1,6 @@
 import pygame
+
+from settings import PLAYER_HEALTH, PLAYER_COULDOWN, PLAYER_SPEED, PLAYER_DOMMAGE, DIRECTION, ARROW_DIRECTION
 from .entity import Entity
 import time
 
@@ -28,37 +30,69 @@ class Player(Entity):
                 pygame.image.load("assets/sprites/Player/droitemarche2.png").convert_alpha(),
             ],
         }
-        super().__init__(x, y, sprites, speed=5)
+        super().__init__(x, y, sprites, PLAYER_SPEED)
         self.projectiles_group = projectiles_group
         self.last_shot_time = 0
-        self.shoot_cooldown = 0.5
-        self.health = 3
-
+        self.shoot_cooldown = PLAYER_COULDOWN
+        self.health = PLAYER_HEALTH
+        self.projectile_direction = DIRECTION.B.value
+        self.projectile_sprite = ARROW_DIRECTION.B.value
 
     def update(self):
         keys = pygame.key.get_pressed()
         dx, dy = 0, 0
 
-        if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+        # Mouvement horizontal
+        if keys[pygame.K_LEFT]or keys[pygame.K_q]:
             dx = -self.speed
-        elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
+        if keys[pygame.K_RIGHT]or keys[pygame.K_d]:
             dx = self.speed
-        if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
+
+        # Mouvement vertical
+        if keys[pygame.K_UP] or keys[pygame.K_z]:
             dy = -self.speed
-        elif keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             dy = self.speed
+
+        # Détermination direction projectile
+        if dx < 0 and dy < 0:
+            self.projectile_direction = DIRECTION.HG.value
+            self.projectile_sprite = ARROW_DIRECTION.HG.value
+        elif dx > 0 and dy < 0:
+            self.projectile_direction = DIRECTION.HD.value
+            self.projectile_sprite = ARROW_DIRECTION.HD.value
+        elif dx < 0 and dy > 0:
+            self.projectile_direction = DIRECTION.BG.value
+            self.projectile_sprite = ARROW_DIRECTION.BG.value
+        elif dx > 0 and dy > 0:
+            self.projectile_direction = DIRECTION.BD.value
+            self.projectile_sprite = ARROW_DIRECTION.BD.value
+        elif dx < 0:
+            self.projectile_direction = DIRECTION.G.value
+            self.projectile_sprite = ARROW_DIRECTION.G.value
+        elif dx > 0:
+            self.projectile_direction = DIRECTION.D.value
+            self.projectile_sprite = ARROW_DIRECTION.D.value
+        elif dy < 0:
+            self.projectile_direction = DIRECTION.H.value
+            self.projectile_sprite = ARROW_DIRECTION.H.value
+        elif dy > 0:
+            self.projectile_direction = DIRECTION.B.value
+            self.projectile_sprite = ARROW_DIRECTION.B.value
+
+        # Tir
         if keys[pygame.K_SPACE]:
             current_time = time.time()
             if current_time - self.last_shot_time > self.shoot_cooldown:
                 self.shoot()
                 self.last_shot_time = current_time
+
+        # Déplacement
         self.move(dx, dy)
 
     def shoot(self):
-        position = self.rect.center
-        velocity = (8, 0)
-        damage = 1
-        arrow = Arrow(position, velocity, damage)
+        print(self.projectile_sprite)
+        arrow = Arrow(self.rect.center, self.projectile_direction, PLAYER_DOMMAGE,self.projectile_sprite)
         shoot_sound = pygame.mixer.Sound("assets/sounds/shoot.ogg")
         shoot_sound.set_volume(1)
         shoot_sound.play()
