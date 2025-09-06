3059ae56d2a663fad018f2d7bbfc194675172469
micheleliot <eliotm38@gmail.com>
2025-09-06 14:38:57 +0200

    add get_direction_vector in utils

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,16 @@
-import pygame
+import math
+import settings  # pour FIREBALL_SPEED
 
-def play_sound(sound_path, volume=1.0):
-    sound = pygame.mixer.Sound(sound_path)
+def play_sound(sound, volume=1.0):
+    """Play a pygame sound with a given volume"""
     sound.set_volume(volume)
     sound.play()
+
+def get_direction_vector(src_x, src_y, dst_x, dst_y, speed):
+    """Return a normalized direction vector scaled by speed"""
+    dx = dst_x - src_x
+    dy = dst_y - src_y
+    distance = math.hypot(dx, dy)
+    if distance == 0:
+        return 0, 0
+    return speed * dx / distance, speed * dy / distance
